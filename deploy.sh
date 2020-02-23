11622b0039a93a92ed7d3cdfa45c6b233e690195
Leonardo Banderali <leonardo2718@protonmail.com>
2020-02-23 12:09:44 -0500

    Create initial website

---
+++ deploy.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/sh
+
+# quit deployment right away if something fails
+set -e
+
+DEPLOY_DIR=public/
+OK="[\033[0;32mOK\033[0m]\n"
+
+if [ $# -ne 1 ]; then
+    echo "Usage: ./deploy.sh COMMIT_MESSAGE"
+    exit 1
+fi
+
+echo "=== Deploying website to GitHub ==="
+
+echo "[Clearing publish dir]"
+cd $DEPLOY_DIR
+git reset --hard
+cd ..
+printf $OK
+
+echo "[Rebuilding website]"
+hugo
+printf $OK
+
+cd $DEPLOY_DIR
+
+echo "[Committing new website]"
+git add .
+git commit -m "$1"
+printf $OK
+
+echo "[Pushing to GitHub]"
+printf "Are you sure (y/n)? "
+read ANSWER
+if [ "$ANSWER" != "y" ]; then
+    echo "ABORTING"
+    exit 1
+fi
+git push origin master
+printf $OK
+
+echo "SUCCESS!!!"
+
